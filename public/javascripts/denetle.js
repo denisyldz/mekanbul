0e57ddc747f817b3d91bdcd1dc324a811150d77a
EsraAydogan4 <esraydgn0842@gmail.com>
2022-10-06 20:14:43 +0300

    deneme

--- public/javascripts/denetle.js
+++ public/javascripts/denetle.js
@@ -1,5 +1,6 @@
 $('#yorumEkle').submit(function (e) {
   $('.alert.alert-danger').hide();
+  
   if (!$('select#rating').val() ||
    !$('textarea#review').val()) {
     if ($('.alert.alert-danger').length) {
