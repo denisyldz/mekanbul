08940c16f8458b4b5d2921e097aa19f75458289c
EsraAydogan4 <esraydgn0842@gmail.com>
2022-10-13 11:19:59 +0300

    hafta5dersi

---
+++ app_server/controllers/mekanlar.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+const anaSayfa=function(req, res, next) {
+    res.render('anasayfa', { title: 'Anasayfa' });
+};
+const mekanBilgisi=function(req, res, next) {
+    res.render('mekanbilgisi', { title: 'mekan bilgisi' });
+};
+const yorumEkle=function(req, res, next) {
+    res.render('yorumekle', { title: 'yorum Ekle' });
+};
+module.exports={
+    anaSayfa,
+    mekanBilgisi,
+    yorumEkle,
+}
